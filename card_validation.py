714f9c5249c5b979c74043c44bc4996158d6b601
Theo Voss <theo.voss973@gmail.com>
2014-04-29 19:38:10 -0400

    Updates for card validation

--- card_validation.py
+++ card_validation.py
@@ -9,19 +9,40 @@ o_name = "card_data/"
 match_inout_name = "good_card_data/"
 unmatch_inout_name = "bad_card_data/"
 
-split_into_files = False
-if split_into_files is True:
+card_id_dictionary = {}
+
+split_into_files = True
+check_files = False
+if check_files is True:
+    for file in listdir(o_name):
+        f=open(o_name+file)
+        text=f.read()
+        text = text.strip()
+        lines = text.split('\n')
+        if len(lines)%2 is 0:
+            print ("even number of events for: " + str(file))
+            print (lines)
+#        else:
+#            print ("odd events for: " + str(file))
+elif split_into_files is True:
     for line in f.readlines():
         files = listdir(o_name)
         myline = line.split(',')
         myid = myline[0]
-        if myid in files:
-            o = open(o_name+myid+".txt",'a+')
-            o.write(line)
+        if myid in card_id_dictionary.keys():
+            card_id_dictionary[myid].append(line)
+        else:
+            card_id_dictionary[myid] = [line]
+
+    for key in card_id_dictionary.keys():
+        if key+".txt" in files:
+            o = open(o_name+key+".txt",'a+')
+            o.write(card_id_dictionary[key])
             o.close()
+#            print ("appended data for " + myid)
         else:
             o = open(o_name+myid+".txt",'w')
-            o.write(line)
+            o.write(card_id_dictionary[key])
             o.close()
 else:
     lot_num_index = 5
@@ -31,9 +52,8 @@ else:
         f = open(o_name + file)
         text = f.read()
         text = text.strip()
-        lines = text.split('\n')
-        ins = re.findall('In',text)
-        outs = re.findall('Out',text)
+        ins = re.findall(',In,',text)
+        outs = re.findall(',Out,',text)
         if len(ins) == len(outs):
             g = open(match_inout_name+file,'w')
             g.write(text)
